c31699a0e68718c997644ad9b221ab18ad152921
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-12-03 18:18:02 -0700

    Improved comments

--- src/_AbstractPredictor.jl
+++ src/_AbstractPredictor.jl
@@ -6,7 +6,7 @@ LinearPredictor(A::AbstractMatrix, B::AbstractMatrix, Σ::Cholesky)
 Linear predictor with added noise covariance Σ (internally a Cholesky factorization is applied)
 Prediction is given by 
 y = Ax + Bu + ε 
-where x and u are vectors and ε is noise
+where x and u are vectors and ε is white noise
 """
 @kwdef struct LinearPredictor{TA<:AbstractArray, TB<:AbstractArray, TΣ<:Cholesky} <: AbstractPredictor
     A :: TA 
@@ -20,15 +20,15 @@ LinearPredictor(A::AbstractMatrix, B::AbstractMatrix, Σ::AbstractMatrix) = Line
 NonlinearPredictor(F::Function, Σ::Cholesky)
 
 Nonlinear predictor with added noise covariance Σ (internally a Cholesky factorization is applied)
-Prediction is given by y = Ax + Bu where x and u are vectors
-
+Prediction is given by 
+y = f(x, u) + ε 
+where x is a vector (u can be any object) and ε is white noise
 """
 @kwdef struct NonlinearPredictor{TF<:Function, TΣ<:Cholesky} <: AbstractPredictor
     f :: TF
     Σ :: TΣ
 end
 NonlinearPredictor(f::Function, Σ::AbstractMatrix) = LinearPredictor(f, cholesky(Σ))
-
 
 const StatePredictor  = Union{LinearPredictor, NonlinearPredictor}
 
