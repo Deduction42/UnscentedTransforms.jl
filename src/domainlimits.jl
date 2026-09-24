4a8777d1365e2d95697864e167b0ef468692cda1
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2026-09-24 17:17:16 -0600

    Updated roadmap, tuned for tests

--- src/domainlimits.jl
+++ src/domainlimits.jl
@@ -198,7 +198,7 @@ end
 
 #Used to find the spread factor δ for a single element of a sigma point, returns a minimum
 function _scale_spread(δ::T, arglims::ArgLimits{N,<:Number}, g::UvGaussian) where {N, T<:Number}
-    ϵ = 0.1
+    ϵ = 1e-6
 
     iszero(g.σ) && return max(ϵ, δ) #Return old value if standard deviation is zero
 
